812a2ef98efe091025e2e85fbfddaf41b431044b
Gargaj <gargaj@conspiracy.hu>
2015-01-22 22:56:27 +0100

    add timer

---
+++ Timer.h
@@ -0,0 +1,5 @@
+namespace Timer
+{
+  void Start();
+  float GetTime();
+}
---
+++ platform_w32_common/Timer.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+
+namespace Timer
+{
+  LARGE_INTEGER LastPCV = { 0 } ;
+  double currentTime = 0.0;
+
+  double _Time()
+  {
+    LARGE_INTEGER count, freq;
+    if (!LastPCV.QuadPart) {
+      QueryPerformanceCounter(&LastPCV);
+    }
+    QueryPerformanceCounter(&count);
+    QueryPerformanceFrequency(&freq);
+
+    currentTime += (double)(count.QuadPart - LastPCV.QuadPart) / (double)(freq.QuadPart);
+
+    LastPCV=count;
+
+    return currentTime * 1000.0f;
+  }
+
+  float startTime = 0.0;
+  void Start()
+  {
+    startTime = (float)_Time();
+  }
+  float GetTime()
+  {
+    return (float)_Time() - startTime;
+  }
+}
