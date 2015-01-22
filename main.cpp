ac2fa057ef03551a46bbfd9a369a8f6ac21020cc
Gargaj <gargaj@conspiracy.hu>
2015-01-22 22:57:08 +0100

    wire in textures + timer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "Renderer.h"
 #include "FFT.h"
+#include "Timer.h"
+#include <Scintilla.h>
 
 void main()
 {
@@ -14,12 +16,29 @@ void main()
   if (!FFT::Open())
     return;
 
+  Renderer::Texture * texChecker = Renderer::CreateRGBA8TextureFromFile("textures/checker.png");
+
+  Renderer::Texture * texFFT = Renderer::Create1DR32Texture( FFT_SIZE );
+
+  Scintilla_LinkLexers();
+
+  Timer::Start();
   while (!Renderer::WantsToQuit())
   {
     Renderer::StartFrame();
 
-    float fftData[FFT_SIZE];
+    float time = Timer::GetTime();
+    Renderer::SetShaderConstant( "fGlobalTime", time );
+
+    Renderer::SetShaderConstant( "v2Resolution", settings.nWidth, settings.nHeight );
+
+    Renderer::SetShaderTexture( "texChecker", texChecker );
+
+    static float fftData[FFT_SIZE];
     FFT::GetFFT(fftData);
+    Renderer::UpdateR32Texture( texFFT, fftData );
+
+    Renderer::SetShaderTexture( "texFFT", texFFT );
 
     Renderer::EndFrame();
   }
