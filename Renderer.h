dde008ee7f13a66b2dea78f6bba0605d78033bf8
Gargaj <gargaj@conspiracy.hu>
2015-01-22 22:57:00 +0100

    add textures

--- Renderer.h
+++ Renderer.h
@@ -14,8 +14,35 @@ typedef struct
 namespace Renderer
 {
   bool Open( RENDERER_SETTINGS * settings );
+  
   void StartFrame();
   void EndFrame();
   bool WantsToQuit();
+
+  void RenderFullscreenQuad();
+
+  bool ReloadShader( char * szShaderCode, int nShaderCodeSize, char * szErrorBuffer, int nErrorBufferSize );
+  void SetShaderConstant( char * szConstName, float x );
+  void SetShaderConstant( char * szConstName, float x, float y );
+
   void Close();
+
+  enum TEXTURETYPE
+  {
+    TEXTURETYPE_1D = 1,
+    TEXTURETYPE_2D = 2,
+  };
+
+  struct Texture
+  {
+    int width;
+    int height;
+    void * ID;
+    TEXTURETYPE type;
+  };
+
+  Texture * CreateRGBA8TextureFromFile( char * szFilename );
+  Texture * Create1DR32Texture( int w );
+  bool UpdateR32Texture( Texture * tex, float * data );
+  void SetShaderTexture( char * szTextureName, Texture * tex );
 }
--- platform_sdl/Renderer.cpp
+++ platform_sdl/Renderer.cpp
@@ -2,16 +2,23 @@
 #include <windows.h>
 #endif
 #include <sdl.h>
-#include <gl/gl.h>
+#include <GLee.h>
+#include <gl\glu.h>
 #include "../Renderer.h"
+
+#define STBI_HEADER_FILE_ONLY
+#include <stb_image.c>
 
 namespace Renderer
 {
-  SDL_Surface* mScreen;
+  SDL_Surface * mScreen = NULL;
   bool run = true;
+
+  GLuint theShader = NULL;
 
   bool Open( RENDERER_SETTINGS * settings )
   {
+    theShader = NULL;
     if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_TIMER ) < 0)
     {
       return false;
@@ -48,6 +55,8 @@ namespace Renderer
 
     return true;
   }
+
+  int textureUnit = 0;
   void StartFrame()
   {
     SDL_Event	E;
@@ -81,6 +90,8 @@ namespace Renderer
     glLoadIdentity();
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
+
+    textureUnit = 1;
   }
   void EndFrame()
   {
@@ -94,4 +105,158 @@ namespace Renderer
   {
     SDL_Quit();
   }
+
+  void RenderFullscreenQuad()
+  {
+    glUseProgram(theShader);
+    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+    glBegin(GL_QUADS);
+      glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.00f, -1.00f);
+      glTexCoord2f(1.0f, 0.0f); glVertex2f( 1.00f, -1.00f);
+      glTexCoord2f(1.0f, 1.0f); glVertex2f( 1.00f,  1.00f);
+      glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.00f,  1.00f);
+    glEnd();
+    glUseProgram(0);
+  }
+
+  bool ReloadShader( char * szShaderCode, int nShaderCodeSize, char * szErrorBuffer, int nErrorBufferSize )
+  {
+    GLuint prg = glCreateProgram();
+    GLuint shd = glCreateShader(GL_FRAGMENT_SHADER);
+    GLint size = 0;
+    GLint result = 0;
+
+    glShaderSource(shd, 1, (const GLchar**)&szShaderCode, &nShaderCodeSize);
+    glCompileShader(shd);
+    glGetShaderInfoLog(shd, nErrorBufferSize, &size, szErrorBuffer);
+    glGetShaderiv(shd, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+      glDeleteProgram(prg);
+      glDeleteShader(shd);
+      return false;
+    }
+
+    glAttachShader(prg, shd);
+    glLinkProgram(prg);
+    glGetProgramInfoLog(prg, nErrorBufferSize - size, &size, szErrorBuffer + size);
+    glGetProgramiv(prg, GL_LINK_STATUS, &result);
+    if (!result)
+    {
+      glDeleteProgram(prg);
+      glDeleteShader(shd);
+      return false;
+    }
+
+    glDeleteProgram(theShader);
+
+    theShader = prg;
+
+    return true;
+  }
+
+  void SetShaderConstant( char * szConstName, float x )
+  {
+    GLint location = glGetUniformLocation( theShader, szConstName );
+    if ( location != -1 )
+    {
+      glProgramUniform1fEXT( theShader, location, x );
+    }
+  }
+
+  void SetShaderConstant( char * szConstName, float x, float y )
+  {
+    GLint location = glGetUniformLocation( theShader, szConstName );
+    if ( location != -1 )
+    {
+      glProgramUniform2fEXT( theShader, location, x, y );
+    }
+  }
+
+  Texture * CreateRGBA8TextureFromFile( char * szFilename )
+  {
+    int comp = 0;
+    int width = 0;
+    int height = 0;
+    unsigned char * c = stbi_load( szFilename, (int*)&width, (int*)&height, &comp, STBI_rgb_alpha );
+    if (!c) return NULL;
+
+    GLuint glTexId = 0;
+    glGenTextures( 1, &glTexId );
+    glBindTexture( GL_TEXTURE_2D, glTexId );
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+    GLenum internalFormat = GL_SRGB8_ALPHA8;
+    GLenum srcFormat = GL_RGBA;
+
+    glTexImage2D( GL_TEXTURE_2D, 0, internalFormat, width, height, 0, srcFormat, GL_UNSIGNED_BYTE, c );
+
+    stbi_image_free(c);
+
+    Texture * tex = new Texture();
+    tex->width = width;
+    tex->height = height;
+    tex->ID = (void *)glTexId;
+    tex->type = TEXTURETYPE_2D;
+    return tex;
+  }
+
+  Texture * Create1DR32Texture( int w )
+  {
+    GLuint glTexId = 0;
+    glGenTextures( 1, &glTexId );
+    glBindTexture( GL_TEXTURE_1D, glTexId );
+
+    glTexParameteri( GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+    glTexParameteri( GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+    float * data = new float[w];
+    for ( int i = 0; i < w; ++i )
+      data[i] = 1.0f;
+
+    glTexImage1D( GL_TEXTURE_1D, 0, GL_R32F, w, 0, GL_RED, GL_FLOAT, data );
+
+    delete[] data;
+
+    glBindTexture( GL_TEXTURE_1D, 0 );
+
+    Texture * tex = new Texture();
+    tex->width = w;
+    tex->height = 1;
+    tex->ID = (void *)glTexId;
+    tex->type = TEXTURETYPE_1D;
+    return tex;
+  }
+
+  void SetShaderTexture( char * szTextureName, Texture * tex )
+  {
+    GLint location = glGetUniformLocation( theShader, szTextureName );
+    if ( location != -1 )
+    {
+      glProgramUniform1iEXT( theShader, location, textureUnit );
+      glActiveTexture( GL_TEXTURE0 + textureUnit );
+      switch( tex->type)
+      {
+        case TEXTURETYPE_1D: glBindTexture( GL_TEXTURE_1D, (GLuint)tex->ID ); break;
+        case TEXTURETYPE_2D: glBindTexture( GL_TEXTURE_2D, (GLuint)tex->ID ); break;
+      }
+      textureUnit++;
+    }
+  }
+
+  bool UpdateR32Texture( Texture * tex, float * data )
+  {
+    int unit = 1; // FIXME
+    glActiveTexture( GL_TEXTURE0 + unit );
+    glBindTexture( GL_TEXTURE_1D, (GLuint)tex->ID );
+    glTexSubImage1D( GL_TEXTURE_1D, 0, 0, tex->width, GL_RED, GL_FLOAT, data );
+
+    return true;
+  }
+
 }
