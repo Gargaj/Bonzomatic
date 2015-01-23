909c71e6b3d95535e6dbccb341251fb0799351fd
Gargaj <gargaj@conspiracy.hu>
2015-01-23 18:43:55 +0100

    minor refactor

--- platform_sdl/Platform.cpp
+++ platform_sdl/Platform.cpp
@@ -378,6 +378,8 @@ struct stbtt_Font
   stbtt_bakedchar cdata[512]; // ASCII 32..126 is 95 glyphs
   GLuint ftex;
   float scale;
+  int w;
+  int h;
 };
 
 Font::Font() : fid(0)
@@ -392,25 +394,26 @@ Font::~Font()
 void Font::Create(const FontParameters &fp)
 {
   stbtt_Font* newFont = new stbtt_Font;
-  size_t len;
-
+  
   FILE* f = fopen(fp.faceName, "rb");
 
   assert(f);
 
   fseek(f, 0, SEEK_END);
-  len = ftell(f);
+  size_t len = ftell(f);
   fseek(f, 0, SEEK_SET);
 
-  int texSize = 1024;
+  int texSize = 512;
   unsigned char* buf = (unsigned char*)malloc(len);
   unsigned char* bmp = new unsigned char[texSize*texSize];
   fread(buf, 1, len, f);
   stbtt_BakeFontBitmap(buf, 0, fp.size, bmp, texSize, texSize, 0, 512, newFont->cdata); // no guarantee this fits!
 
+  newFont->w = texSize;
+  newFont->h = texSize;
 #ifdef _DEBUG
   FILE* dump = fopen("font.raw", "wb");
-  fwrite(bmp,1024,1024,dump);
+  fwrite(bmp,texSize,texSize,dump);
   fclose(dump);
 #endif // _DEBUG
 
@@ -475,7 +478,7 @@ void SurfaceImpl::DrawTextBase(PRectangle rc, Font &font_, float ybase, const ch
       UTF16FromUTF8( s, l, (wchar_t*)&c, sizeof(unsigned int) );
     }
     stbtt_aligned_quad q;
-    stbtt_GetBakedQuad(realFont->cdata, 1024,1024, c, &x,&y,&q,1);//1=opengl,0=old d3d
+    stbtt_GetBakedQuad(realFont->cdata, realFont->w,realFont->h, c, &x,&y,&q,1);//1=opengl,0=old d3d
     //x = floor(x);
     glTexCoord2f(q.s0,q.t0); glVertex2f(q.x0,q.y0);
     glTexCoord2f(q.s1,q.t0); glVertex2f(q.x1,q.y0);
