537ea729feee9fd5c4bd9cee5e91ac76fc74e7b5
Gargaj <gargaj@conspiracy.hu>
2015-01-31 22:23:30 +0100

    gut out old dx9 code

--- platform_w32_dx11/Renderer.cpp
+++ platform_w32_dx11/Renderer.cpp
@@ -101,11 +101,9 @@ namespace Renderer
 
   bool run = true;
 
-  LPDIRECT3D9 pD3D = NULL;
-  LPDIRECT3DDEVICE9 pDevice = NULL;
-  LPD3DXCONSTANTTABLE pConstantTable = NULL;
-  LPDIRECT3DVERTEXSHADER9 pVertexShader = NULL;
-  LPDIRECT3DPIXELSHADER9 theShader = NULL;
+  IDXGISwapChain * pSwapChain;
+  ID3D11Device * pDevice;
+  ID3D11DeviceContext * pContext;
 
   int nWidth = 0;
   int nHeight = 0;
@@ -297,7 +295,7 @@ namespace Renderer
     RECT wr={0,0,pSetup->nWidth,pSetup->nHeight};
     AdjustWindowRectEx(&wr, wStyle, FALSE, wExStyle);
 
-    hWnd = CreateWindowEx(wExStyle,_T("fwzwnd"),_T("BONZOMATIC - Direct3D 9.0c edition"),wStyle,
+    hWnd = CreateWindowEx(wExStyle,_T("fwzwnd"),_T("BONZOMATIC - Direct3D 11.0 edition"),wStyle,
       (GetSystemMetrics(SM_CXSCREEN) - pSetup->nWidth )/2,
       (GetSystemMetrics(SM_CYSCREEN) - pSetup->nHeight)/2,
       wr.right-wr.left, wr.bottom-wr.top,
@@ -315,96 +313,37 @@ namespace Renderer
 
   bool InitDirect3D(RENDERER_SETTINGS * pSetup) 
   {
-    pD3D=Direct3DCreate9(D3D9b_SDK_VERSION);
-    if (!pD3D) return false;
+    DXGI_SWAP_CHAIN_DESC desc;
 
-    nWidth  = pSetup->nWidth;
-    nHeight = pSetup->nHeight;
+    // clear out the struct for use
+    ZeroMemory(&desc, sizeof(DXGI_SWAP_CHAIN_DESC));
 
-    D3DPRESENT_PARAMETERS d3dpp;
-    ZeroMemory(&d3dpp,sizeof(d3dpp));
+    // fill the swap chain description struct
+    desc.BufferCount = 1;
+    desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    desc.OutputWindow = hWnd;
+    desc.SampleDesc.Count = 4;
+    desc.Windowed = pSetup->windowMode != RENDERER_WINDOWMODE_FULLSCREEN;                                  
 
-    d3dpp.SwapEffect     = D3DSWAPEFFECT_DISCARD;
-    d3dpp.hDeviceWindow  = hWnd;
-    d3dpp.Windowed       = pSetup->windowMode != RENDERER_WINDOWMODE_FULLSCREEN;
-    d3dpp.PresentationInterval = pSetup->bVsync ? D3DPRESENT_INTERVAL_ONE : D3DPRESENT_INTERVAL_IMMEDIATE;
-
-    d3dpp.BackBufferCount  = 1;
-
-    d3dpp.BackBufferWidth  = pSetup->nWidth;
-    d3dpp.BackBufferHeight = pSetup->nHeight;
-
-    D3DDISPLAYMODE d3ddm;
-    if( FAILED( pD3D->GetAdapterDisplayMode( D3DADAPTER_DEFAULT, &d3ddm ) ) )
+    // create a device, device context and swap chain using the information in the scd struct
+    if (D3D11CreateDeviceAndSwapChain(
+      NULL,
+      D3D_DRIVER_TYPE_HARDWARE,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      D3D11_SDK_VERSION,
+      &desc,
+      &pSwapChain,
+      &pDevice,
+      NULL,
+      &pContext) != S_OK)
       return false;
 
-    static D3DFORMAT pBackbufferFormats32[] = {
-      D3DFMT_X8R8G8B8,
-      D3DFMT_A8R8G8B8,
-      D3DFMT_UNKNOWN
-    };
-    D3DFORMAT * pFormats = pBackbufferFormats32;
-    for (int i=0; pFormats[i] != D3DFMT_UNKNOWN; i++)
-      if ( SUCCEEDED(pD3D->CheckDeviceType( D3DADAPTER_DEFAULT, DEVTYPE, d3ddm.Format, pFormats[i], d3dpp.Windowed )) )
-        d3dpp.BackBufferFormat = pFormats[i];
-
-    if (d3dpp.BackBufferFormat == D3DFMT_UNKNOWN) 
-      return false;
-
-    d3dpp.EnableAutoDepthStencil = TRUE;
-    d3dpp.AutoDepthStencilFormat = D3DFMT_UNKNOWN;
-
-    static D3DFORMAT pDepthFormats[] = {
-      D3DFMT_D16,
-      D3DFMT_D24X8,
-      D3DFMT_D32,
-      D3DFMT_UNKNOWN
-    };
-    for (int i=0; pDepthFormats[i]!=D3DFMT_UNKNOWN; i++)
-      if ( SUCCEEDED(pD3D->CheckDeviceFormat( D3DADAPTER_DEFAULT, DEVTYPE, d3ddm.Format, D3DUSAGE_DEPTHSTENCIL, D3DRTYPE_SURFACE, pDepthFormats[i] )) )
-        d3dpp.AutoDepthStencilFormat = pDepthFormats[i];
-
-    if (d3dpp.AutoDepthStencilFormat == D3DFMT_UNKNOWN) 
-      return false;
-
-    static D3DMULTISAMPLE_TYPE pMultisampleTypes[]=
-    {
-      D3DMULTISAMPLE_2_SAMPLES,
-      D3DMULTISAMPLE_4_SAMPLES,
-      D3DMULTISAMPLE_6_SAMPLES,
-      D3DMULTISAMPLE_8_SAMPLES,
-      D3DMULTISAMPLE_NONE
-    };
-
-    d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
-
-    HRESULT h;
-    h = pD3D->CreateDevice( D3DADAPTER_DEFAULT, DEVTYPE, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &pDevice);
-    if (h != D3D_OK) 
-    {
-      h = pD3D->CreateDevice( D3DADAPTER_DEFAULT, DEVTYPE, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pDevice);
-      if(h != D3D_OK) 
-      {
-        return false;
-      }
-    }
-
-    pDevice->SetRenderState( D3DRS_ZENABLE , D3DZB_FALSE );
-    pDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_NONE );
-    pDevice->SetRenderState( D3DRS_LIGHTING, FALSE );
-
-    for (int x=0; x<4; x++) 
-    {
-      pDevice->SetSamplerState( x, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
-      pDevice->SetSamplerState( x, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
-      pDevice->SetSamplerState( x, D3DSAMP_MIPFILTER, D3DTEXF_NONE);
-    }
-    return 1;
+    return true;
   }
-
-  LPDIRECT3DVERTEXBUFFER9 pFullscreenQuadVB = NULL;
-  LPDIRECT3DVERTEXBUFFER9 pGUIQuadVB = NULL;
-  LPDIRECT3DVERTEXDECLARATION9 pPostProcessVertexDecl = NULL;
 
 #define GUIQUADVB_SIZE (128*6)
 
@@ -416,68 +355,14 @@ namespace Renderer
     if (!InitDirect3D(settings))
       return false;
 
-    static D3DVERTEXELEMENT9 pFullscreenQuadElements[] = 
-    {
-      { 0, 0*sizeof(float), D3DDECLTYPE_FLOAT3, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_POSITION, 0 },
-      { 0, 3*sizeof(float), D3DDECLTYPE_FLOAT2, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_TEXCOORD, 0 },
-      D3DDECL_END()
-    };
-
-    static float pQuad[] = {
-      -1.0, -1.0,  0.0, 0.0, 0.0,
-       1.0, -1.0,  0.0, 1.0, 0.0,
-      -1.0,  1.0,  0.0, 0.0, 1.0,
-       1.0,  1.0,  0.0, 1.0, 1.0,
-    };
-
-    pDevice->CreateVertexBuffer( 4 * 5 * sizeof(float), D3DUSAGE_WRITEONLY, D3DFVF_XYZ | D3DFVF_TEX1, D3DPOOL_DEFAULT, &pFullscreenQuadVB, NULL);
-    void * v;
-    pFullscreenQuadVB->Lock( 0, 4 * 5 * sizeof(float), &v, NULL );
-    CopyMemory( v, pQuad, 4 * 5 * sizeof(float) );
-    pFullscreenQuadVB->Unlock();
-
-    pDevice->CreateVertexDeclaration( pFullscreenQuadElements, &pPostProcessVertexDecl );
-
-    //////////////////////////////////////////////////////////////////////////
-
-    pDevice->CreateVertexBuffer( GUIQUADVB_SIZE * 6 * sizeof(float), D3DUSAGE_WRITEONLY, D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1, D3DPOOL_DEFAULT, &pGUIQuadVB, NULL);
-
-    //////////////////////////////////////////////////////////////////////////
-
-    LPD3DXBUFFER pShader = NULL;
-    LPD3DXBUFFER pErrors = NULL;
-
-    if (D3DXCompileShader( defaultVertexShader, strlen(defaultVertexShader), NULL, NULL, "main", "vs_3_0", NULL, &pShader, &pErrors, NULL ) != D3D_OK)
-    {
-      return false;
-    }
-
-    if (pDevice->CreateVertexShader( (DWORD*)pShader->GetBufferPointer(), &pVertexShader ) != D3D_OK)
-    {
-      return false;
-    }
-
     return true;
   }
 
-  unsigned int nCacheFlushCount = 0;
   void StartFrame()
   {
-    nCacheFlushCount = 0;
-    MSG msg;
-    if( PeekMessage( &msg, NULL, 0U, 0U, PM_REMOVE ) ) 
-    {
-      TranslateMessage( &msg );
-      DispatchMessage( &msg );
-    }
-
-    pDevice->Clear( 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER , 0xFF808080, 1.0f, 0 );
-    pDevice->BeginScene();
   }
   void EndFrame()
   {
-    pDevice->EndScene();
-    pDevice->Present( NULL, NULL, NULL, NULL );
   }
   bool WantsToQuit()
   {
@@ -485,8 +370,9 @@ namespace Renderer
   }
   void Close()
   {
+    if (pContext) pContext->Release();
+    if (pSwapChain) pSwapChain->Release();
     if (pDevice) pDevice->Release();
-    if (pD3D) pD3D->Release();
     if (!hWnd) 
     {
       DestroyWindow(hWnd);
@@ -496,169 +382,49 @@ namespace Renderer
 
   void RenderFullscreenQuad()
   {
-    pDevice->SetRenderState( D3DRS_ALPHABLENDENABLE, false );
-
-    D3DXMATRIX mat;
-    D3DXMatrixIdentity( &mat );
-    pDevice->SetTransform( D3DTS_VIEW, &mat );
-    pDevice->SetTransform( D3DTS_WORLD, &mat );
-    //D3DXMatrixOrthoOffCenterLH( (D3DXMATRIX*)&mat, -1.0f, -1.0, 1.0f, 1.0f, -1.0f, 1.0f );
-    //D3DXMatrixOrthoOffCenterLH( &mat, -1.0, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f );
-    pDevice->SetTransform( D3DTS_PROJECTION, &mat );
-
-    pDevice->SetVertexShader( pVertexShader );
-    pDevice->SetPixelShader( theShader );
-    
-    pDevice->SetVertexDeclaration( pPostProcessVertexDecl );
-    pDevice->SetStreamSource( 0, pFullscreenQuadVB, 0, sizeof(float) * 5 ); 
-    pDevice->DrawPrimitive( D3DPT_TRIANGLESTRIP, 0, 2 );
   }
 
   bool ReloadShader( char * szShaderCode, int nShaderCodeSize, char * szErrorBuffer, int nErrorBufferSize )
   {
-    LPD3DXBUFFER pShader = NULL;
-    LPD3DXBUFFER pErrors = NULL;
-
-    if (D3DXCompileShader( szShaderCode, nShaderCodeSize, NULL, NULL, "main", "ps_3_0", NULL, &pShader, &pErrors, &pConstantTable ) != D3D_OK)
-    {
-      memset( szErrorBuffer, 0, nErrorBufferSize );
-      strncpy( szErrorBuffer, (char*)pErrors->GetBufferPointer(), nErrorBufferSize - 1 );
-      return false;
-    }
-
-    if (theShader) 
-    {
-      theShader->Release();
-      theShader = NULL;
-    }
-
-    if (pDevice->CreatePixelShader( (DWORD*)pShader->GetBufferPointer(), &theShader ) != D3D_OK)
-    {
-      return false;
-    }
-
     return true;
   }
 
   void SetShaderConstant( char * szConstName, float x )
   {
-    pConstantTable->SetFloat( pDevice, szConstName, x );
   }
 
   void SetShaderConstant( char * szConstName, float x, float y )
   {
-    pConstantTable->SetVector( pDevice, szConstName, &D3DXVECTOR4(x, y, 0, 0) );
   }
 
-  struct DX9Texture : public Texture
+  struct DX11Texture : public Texture
   {
-    LPDIRECT3DTEXTURE9 pTexture;
+    ID3D11Resource * pTexture;
   };
 
   int textureUnit = 0;
   Texture * CreateRGBA8TextureFromFile( char * szFilename )
   {
-    LPDIRECT3DTEXTURE9 pTex = NULL;
-    D3DXIMAGE_INFO info;
-    HRESULT h = D3DXCreateTextureFromFileExA(
-      pDevice,
-      szFilename,
-      D3DX_DEFAULT,
-      D3DX_DEFAULT,
-      0,
-      NULL,
-      D3DFMT_FROM_FILE,
-      D3DPOOL_DEFAULT,
-      D3DX_FILTER_NONE,
-      D3DX_DEFAULT,
-      NULL,
-      &info,
-      NULL,
-      &pTex);
-
-    if (!pTex)
-      return NULL;
-
-    DX9Texture * tex = new DX9Texture();
-    tex->pTexture = pTex;
-    tex->width = info.Width;
-    tex->height = info.Height;
-    tex->type = TEXTURETYPE_2D;
-    return tex;
+    return NULL;
   }
 
   Texture * Create1DR32Texture( int w )
   {
-    LPDIRECT3DTEXTURE9 pTex = NULL;
-    pDevice->CreateTexture( w, 1, 0, D3DUSAGE_DYNAMIC, D3DFMT_R32F, D3DPOOL_DEFAULT, &pTex, NULL );
-
-    if (!pTex)
-      return NULL;
-
-    D3DLOCKED_RECT rect;
-    pTex->LockRect( 0, &rect, NULL, NULL );
-    memset( rect.pBits, 0, w * sizeof(float) );
-    pTex->UnlockRect(0);
-
-    DX9Texture * tex = new DX9Texture();
-    tex->pTexture = pTex;
-    tex->width = w;
-    tex->height = 1;
-    tex->type = TEXTURETYPE_1D;
-    return tex;
+    return NULL;
   }
 
   void SetShaderTexture( char * szTextureName, Texture * tex )
   {
-    int idx = pConstantTable->GetSamplerIndex( szTextureName );
-    if (idx >= 0)
-    {
-      pDevice->SetSamplerState( idx, D3DSAMP_SRGBTEXTURE, TRUE );
-      pDevice->SetSamplerState( idx, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP );
-      pDevice->SetSamplerState( idx, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP );
-      pDevice->SetTexture( idx, ((DX9Texture *)tex)->pTexture );
-    }
   }
 
   bool UpdateR32Texture( Texture * tex, float * data )
   {
-    LPDIRECT3DTEXTURE9 pTex = ((DX9Texture *)tex)->pTexture;
-    
-    D3DLOCKED_RECT rect;
-    pTex->LockRect( 0, &rect, NULL, D3DLOCK_DISCARD );
-    memcpy( rect.pBits, data, tex->width * sizeof(float) );
-    pTex->UnlockRect(0);
-
     return true;
   }
 
   Texture * CreateA8TextureFromData( int w, int h, unsigned char * data )
   {
-    LPDIRECT3DTEXTURE9 pTex = NULL;
-    pDevice->CreateTexture( w, h, 0, NULL, D3DFMT_A8, D3DPOOL_MANAGED, &pTex, NULL );
-
-    if (!pTex)
-      return NULL;
-
-    D3DLOCKED_RECT rect;
-    pTex->LockRect( 0, &rect, NULL, NULL );
-    //memset( rect.pBits, 0, w * sizeof(float) );
-    unsigned char * src = data;
-    unsigned char * dst = (unsigned char *)rect.pBits;
-    for (int i=0; i<h; i++)
-    {
-      memcpy( dst, src, w * sizeof(unsigned char) );
-      src += w * sizeof(unsigned char);
-      dst += rect.Pitch;
-    }
-    pTex->UnlockRect(0);
-
-    DX9Texture * tex = new DX9Texture();
-    tex->pTexture = pTex;
-    tex->width = w;
-    tex->height = h;
-    tex->type = TEXTURETYPE_1D;
-    return tex;
+    return NULL;
   }
 
   //////////////////////////////////////////////////////////////////////////
@@ -667,32 +433,11 @@ namespace Renderer
 
   void StartTextRendering()
   {
-    pDevice->SetVertexShader( NULL );
-    pDevice->SetPixelShader( NULL );
-    pDevice->SetRenderState( D3DRS_SCISSORTESTENABLE, TRUE );
-    //pDevice->SetVertexDeclaration( pGUIVertexDecl );
-    pDevice->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1);
-    pDevice->SetRenderState( D3DRS_ALPHABLENDENABLE, TRUE );
-    pDevice->SetRenderState( D3DRS_SRCBLEND, D3DBLEND_SRCALPHA );
-    pDevice->SetRenderState( D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA );
-
-    D3DXMATRIX mat;
-    D3DXMatrixIdentity( &mat );
-    pDevice->SetTransform( D3DTS_VIEW, &mat );
-    pDevice->SetTransform( D3DTS_WORLD, &mat );
-    D3DXMatrixOrthoOffCenterLH( (D3DXMATRIX*)&mat, 0, nWidth, nHeight, 0, -1.0f, 1.0f );
-    pDevice->SetTransform( D3DTS_PROJECTION, &mat );
-
-    pDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_SELECTARG1 );
-    pDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_DIFFUSE );
-    pDevice->SetTextureStageState( 0, D3DTSS_ALPHAOP, D3DTOP_MODULATE );
-    pDevice->SetTextureStageState( 0, D3DTSS_ALPHAARG1, D3DTA_DIFFUSE );
-    pDevice->SetTextureStageState( 0, D3DTSS_ALPHAARG2, D3DTA_TEXTURE );
   }
 
   void ReleaseTexture( Texture * tex )
   {
-    ((DX9Texture *)tex)->pTexture->Release();
+    ((DX11Texture *)tex)->pTexture->Release();
     delete tex;
   }
 
@@ -703,7 +448,7 @@ namespace Renderer
   {
     if (!bufferPointer) return;
 
-    nCacheFlushCount++;
+/*
     void * v = NULL;
     pGUIQuadVB->Lock( 0, bufferPointer * sizeof(float) * 6, &v, NULL );
     CopyMemory( v, buffer, bufferPointer * sizeof(float) * 6 );
@@ -714,6 +459,7 @@ namespace Renderer
       pDevice->DrawPrimitive( D3DPT_TRIANGLELIST, 0, bufferPointer / 3 );
     else
       pDevice->DrawPrimitive( D3DPT_LINELIST, 0, bufferPointer / 2 );
+*/
 
     bufferPointer = 0;
   }
@@ -744,7 +490,7 @@ namespace Renderer
     {
       __FlushRenderCache();
       lastTexture = tex;
-      pDevice->SetTexture( 0, tex ? ((DX9Texture *)tex)->pTexture : NULL );
+//      pDevice->SetTexture( 0, tex ? ((DX9Texture *)tex)->pTexture : NULL );
     }
   }
 
@@ -776,6 +522,7 @@ namespace Renderer
 
   void SetTextRenderingViewport( Scintilla::PRectangle rect )
   {
+/*
     __FlushRenderCache();
     D3DXMATRIX mat;
     D3DXMatrixIdentity( &mat );
@@ -785,12 +532,15 @@ namespace Renderer
 
     RECT rc = { rect.left, rect.top, rect.right, rect.bottom };
     pDevice->SetScissorRect( &rc );
+*/
   }
   void EndTextRendering()
   {
+/*
     __FlushRenderCache();
     pDevice->SetRenderState( D3DRS_SCISSORTESTENABLE, false );
     pDevice->SetRenderState( D3DRS_ALPHABLENDENABLE, false );
+*/
   }
 
 }
