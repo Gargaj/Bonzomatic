79df049206b5dda9cde720f6187fd63f580f87c0
Gargaj <gargaj@conspiracy.hu>
2015-01-31 02:53:48 +0100

    error check

--- platform_sdl/Renderer.cpp
+++ platform_sdl/Renderer.cpp
@@ -174,9 +174,9 @@ namespace Renderer
     SDL_GL_SetAttribute( SDL_GL_STENCIL_SIZE, 8 );
     SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, true );
 
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
+    SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 4 );
 
     nWidth = settings->nWidth;
     nHeight = settings->nHeight;
@@ -189,7 +189,8 @@ namespace Renderer
     if (!mWindow)
       return false;
 
-    SDL_GL_CreateContext(mWindow);
+    if (!SDL_GL_CreateContext(mWindow))
+      return false;
 
 #ifdef _WIN32
     if (settings->bVsync)
@@ -373,7 +374,8 @@ namespace Renderer
       return false;
     }
 
-    glDeleteProgram(theShader);
+    if (theShader)
+      glDeleteProgram(theShader);
 
     theShader = prg;
 
