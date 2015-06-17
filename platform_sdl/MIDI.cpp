bce16034fba6ac1aa3ccee66f9285b4125bbf41e
Urs Ganse <urs.ganse@helsinki.fi>
2015-06-17 17:07:56 +0300

    Added dummy midi and misc-functions to platform_sdl, fixing linux build.

---
+++ platform_sdl/MIDI.cpp
@@ -0,0 +1,14 @@
+// Dummy midi implementation, just to make sure bonzomatic compiles.
+#include "../MIDI.h"
+
+bool MIDI::Open() {
+	return false;
+}
+
+bool MIDI::Close() {
+	return false;
+}
+
+float MIDI::GetCCValue( unsigned char cc) {
+	return 0.f;
+}
