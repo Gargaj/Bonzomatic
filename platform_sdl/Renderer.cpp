304df1f5d60d6ab3a595ba154b4231ab6e307403
ferris <jake@fusetools.com>
2016-05-08 14:24:43 -0700

    Request GL 4.1 core profile

--- platform_sdl/Renderer.cpp
+++ platform_sdl/Renderer.cpp
@@ -201,9 +201,9 @@ namespace Renderer
     SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, true );
 
     SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1 );
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 1 );
 
     nWidth = settings->nWidth;
     nHeight = settings->nHeight;
