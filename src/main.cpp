d6c93ac315780aab0a43b6e3b11e9485d4bd81f5
Gargaj <gargaj@conspiracy.hu>
2016-05-24 01:24:26 +0200

    add ndi options + async (threaded) transfer

--- src/main.cpp
+++ src/main.cpp
@@ -129,6 +129,9 @@ int main()
   float fFFTSmoothingFactor = 0.9f; // higher value, smoother FFT
 
   std::string sNDIConnectionString;
+  float fNDIFrameRate = 60.0;
+  std::string sNDIIdentifier;
+  bool bNDIProgressive = true;
 
   char szConfig[65535];
   FILE * fConf = fopen("config.json","rb");
@@ -200,6 +203,12 @@ int main()
     {
       if (o.get<jsonxx::Object>("ndi").has<jsonxx::String>("connectionString"))
         sNDIConnectionString = o.get<jsonxx::Object>("ndi").get<jsonxx::String>("connectionString");
+      if (o.get<jsonxx::Object>("ndi").has<jsonxx::String>("identifier"))
+        sNDIIdentifier = o.get<jsonxx::Object>("ndi").get<jsonxx::String>("identifier");
+      if (o.get<jsonxx::Object>("ndi").has<jsonxx::Number>("frameRate"))
+        fNDIFrameRate = o.get<jsonxx::Object>("ndi").get<jsonxx::Number>("frameRate");
+      if (o.get<jsonxx::Object>("ndi").has<jsonxx::Boolean>("progressive"))
+        bNDIProgressive = o.get<jsonxx::Object>("ndi").get<jsonxx::Boolean>("progressive");
     }
   }
 
@@ -279,6 +288,8 @@ int main()
   NDIlib_video_frame_t pNDIFrame;
   NDIlib_send_instance_t pNDI_send;
   bool bNDIEnabled = false;
+  unsigned int * pBuffer[2] = { NULL, NULL };
+  unsigned int nBufferIndex = 0;
   if (sNDIConnectionString.length())
   {
     if (!NDIlib_initialize())
@@ -287,9 +298,14 @@ int main()
       return 0;
     }
 
-    const NDIlib_send_create_t NDI_send_create_desc = { "Bonzomatic NDI source", NULL, TRUE, FALSE };
+    NDIlib_send_create_t pNDICreateDesc;
+    sNDIIdentifier = "BONZOMATIC" + (sNDIIdentifier.length() ? (" - " + sNDIIdentifier) : "");
+    pNDICreateDesc.p_ndi_name = sNDIIdentifier.c_str();
+    pNDICreateDesc.p_groups = NULL;
+    pNDICreateDesc.clock_video = TRUE;
+    pNDICreateDesc.clock_audio = FALSE;
 
-    pNDI_send = NDIlib_send_create(&NDI_send_create_desc);
+    pNDI_send = NDIlib_send_create(&pNDICreateDesc);
     if (!pNDI_send)
     {
       printf("Cannot create NDI source");
@@ -306,12 +322,14 @@ int main()
     pNDIFrame.xres = settings.nWidth;
     pNDIFrame.yres = settings.nHeight;
     pNDIFrame.FourCC = NDIlib_FourCC_type_BGRA;
-    pNDIFrame.frame_rate_N = 30000;
-    pNDIFrame.frame_rate_D = 1001;
+    pNDIFrame.frame_rate_N = fNDIFrameRate * 100;
+    pNDIFrame.frame_rate_D = 100;
     pNDIFrame.picture_aspect_ratio = settings.nWidth / (float)settings.nHeight;
-    pNDIFrame.is_progressive = false;
+    pNDIFrame.is_progressive = bNDIProgressive;
     pNDIFrame.timecode = NDIlib_send_timecode_synthesize;
-    pNDIFrame.p_data = (BYTE*)malloc(settings.nWidth * settings.nHeight * 4);
+    pBuffer[0] = new unsigned int[settings.nWidth * settings.nHeight * 4];
+    pBuffer[1] = new unsigned int[settings.nWidth * settings.nHeight * 4];
+    pNDIFrame.p_data = NULL;
     pNDIFrame.line_stride_in_bytes = settings.nWidth * 4;
 
     bNDIEnabled = true;
@@ -485,18 +503,26 @@ int main()
 
     Renderer::EndFrame();
 
-    if (bNDIEnabled && Renderer::GrabFrame( pNDIFrame.p_data ))
+    if (bNDIEnabled)
     {
-      unsigned int * p = (unsigned int *)pNDIFrame.p_data;
-      for(int i=0; i < settings.nWidth * settings.nHeight; i++)
-        p[i] = (p[i] & 0x00FF00) | ((p[i] >> 16) & 0xFF) | ((p[i] & 0xFF) << 16) | 0xFF000000;
-      NDIlib_send_send_video(pNDI_send, &pNDIFrame);
+      pNDIFrame.p_data = (BYTE*)pBuffer[ nBufferIndex ];
+      nBufferIndex = (nBufferIndex + 1) & 1;
+      if (Renderer::GrabFrame( pNDIFrame.p_data ))
+      {
+        unsigned int * p = (unsigned int *)pNDIFrame.p_data;
+        for(int i=0; i < settings.nWidth * settings.nHeight; i++)
+          p[i] = (p[i] & 0x00FF00) | ((p[i] >> 16) & 0xFF) | ((p[i] & 0xFF) << 16) | 0xFF000000;
+        NDIlib_send_send_video_async(pNDI_send, &pNDIFrame);
+      }
     }
   }
 
   if (bNDIEnabled)
   {
-    free((void*)pNDIFrame.p_data);
+    NDIlib_send_send_video_async(pNDI_send, NULL); // stop async thread
+
+    delete[] pBuffer[0];
+    delete[] pBuffer[1];
     NDIlib_send_destroy(pNDI_send);
     NDIlib_destroy();
   }
