2cf3c6a6036a5e711d032449b570128b5ee5b7de
Patrick Labatut <plabatut@gmail.com>
2017-05-03 18:53:08 +0200

    Make OpenSetupDialog really do nothing on GLFW platforms.

--- src/platform_x11/SetupDialog.cpp
+++ src/platform_x11/SetupDialog.cpp
@@ -3,9 +3,5 @@
 
 bool Renderer::OpenSetupDialog( RENDERER_SETTINGS * settings )
 {
-  std::cerr << __FUNCTION__ << " STUB" << std::endl;
-  settings->nWidth = 1280;
-  settings->nHeight = 720;
-  settings->windowMode = RENDERER_WINDOWMODE_WINDOWED;
   return true;
 }
