da150c9b11c67b06c327b735a07c67288047b0d9
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2018-11-20 22:20:12 +0100

    fixed truetype fonts paths for linux + bring back TTF paths

--- src/platform_x11/Misc.cpp
+++ src/platform_x11/Misc.cpp
@@ -40,12 +40,15 @@ const char * Misc::GetDefaultFontPath()
   // TODO: use fonts.conf(5) or X resources or something like that
   const char* fontPaths[] =
   {
-    "/usr/share/fonts/truetype/freefont/DejaVuSansMono.ttf",
+    "/usr/share/fonts/TTF/DejaVuSansMono.ttf",
+    "/usr/share/fonts/TTF/FreeMono.ttf",
+    "/usr/share/fonts/TTF/LiberationMono-Regular.ttf",
+    "/usr/share/fonts/TTF/VeraMono.ttf",
+    "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf",
     "/usr/share/fonts/truetype/freefont/FreeMono.ttf",
-    "/usr/share/fonts/truetype/freefont/LiberationMono-Regular.ttf",
-    "/usr/share/fonts/truetype/freefont/VeraMono.ttf",
-    "/usr/share/fonts/corefonts/cour.ttf",
+    "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf",
     "/usr/share/fonts/truetype/msttcorefonts/cour.ttf",
+    "/usr/share/fonts/corefonts/cour.ttf",
     NULL
   };
   for (int i = 0; fontPaths[i]; ++i)
