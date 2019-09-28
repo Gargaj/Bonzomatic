27f2f914ae93aa4b16b719406a527314a6140792
Tomas Heran <tomas.heran@gmail.com>
2019-09-28 13:39:43 +0200

    Add support for reading font.conf on Linux using tinyxml2

--- src/platform_x11/Misc.cpp
+++ src/platform_x11/Misc.cpp
@@ -5,6 +5,11 @@
 #include "../Misc.h"
 
 #include <unistd.h>
+#include <dirent.h>
+
+#include <tinyxml2.h>
+
+using namespace tinyxml2;
 
 void Misc::PlatformStartup()
 {
@@ -16,17 +21,17 @@ void Misc::PlatformShutdown()
 
 void Misc::InitKeymaps()
 {
-	return;
+  return;
 }
 
 void Misc::GetKeymapName(char* sz)
 {
-	strncpy(sz,"<native>",7);
+  strncpy(sz,"<native>",7);
 }
 
 bool Misc::ExecuteCommand( const char * cmd, const char * param )
 {
-	return false;
+  return false;
 }
 
 bool Misc::FileExists(const char * path)
@@ -34,10 +39,63 @@ bool Misc::FileExists(const char * path)
   return access(path, R_OK) != -1;
 }
 
+static char * ScanFontDir(const char * path, const char **fontNames)
+{
+  if (!Misc::FileExists(path)) {
+    return NULL;
+  }
+
+  DIR *d = opendir(path);
+  if (d == NULL) {
+    return NULL;
+  }
+  struct dirent *de;
+
+  while ((de = readdir(d)) != NULL) {
+    if (strcmp(de->d_name, ".") == 0 ||
+        strcmp(de->d_name, "..") == 0) {
+      continue;
+    }
+    if (de->d_type == DT_REG) {
+      for (int i = 0; fontNames[i] != NULL; i++) {
+        if (strcmp(de->d_name, fontNames[i]) == 0) {
+          char *ret;
+          (void) asprintf(&ret, "%s/%s", path, fontNames[i]);
+          closedir(d);
+          return ret;
+        }
+      }
+    } else if (de->d_type == DT_DIR) {
+      char *subdir;
+      (void) asprintf(&subdir, "%s/%s", path, de->d_name);
+      char *subdir_match = ScanFontDir(subdir, fontNames);
+      free(subdir);
+      if (subdir_match != NULL) {
+        closedir(d);
+        return subdir_match;
+      }
+    }
+  }
+
+  closedir(d);
+  return NULL;
+}
+
 const char * Misc::GetDefaultFontPath()
 {
   // Linux case
-  // TODO: use fonts.conf(5) or X resources or something like that
+  // font names we're looking for
+  const char *fontNames[] =
+  {
+    "DejaVuSansMono.ttf",
+    "FreeMono.ttf",
+    "LiberationMono-Regular.ttf",
+    "UbuntuMono-R.ttf",
+    "VeraMono.ttf",
+    "cour.ttf",
+    NULL
+  };
+  // full paths we're looking for first
   const char* fontPaths[] =
   {
     "/usr/share/fonts/TTF/DejaVuSansMono.ttf",
@@ -59,5 +117,32 @@ const char * Misc::GetDefaultFontPath()
       return fontPaths[i];
     }
   }
+
+  /*
+   * Fast lookup didn't work, let's read fonts.conf and
+   * let's start scanning directories
+   */
+
+  XMLDocument fontsconf;
+  if (fontsconf.LoadFile("/etc/fonts/fonts.conf") != XML_SUCCESS) {
+    return NULL;
+  }
+
+  XMLNode *d = fontsconf.RootElement()->FirstChildElement("dir");
+  if (d == NULL) {
+    return NULL;
+  }
+
+  while (d != NULL) {
+   char *tmp = ScanFontDir((const char *)d->FirstChild()->Value(), fontNames);
+    if (tmp != NULL) {
+      static char fontPath[PATH_MAX] = { 0 };
+      strcpy(fontPath, tmp);
+      free(tmp);
+      return fontPath;
+    }
+    d = d->NextSiblingElement("dir");
+  }
+
   return NULL;
 }
