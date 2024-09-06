9b79b1ec5bcd6a15e08ac2a564494a94b58e975e
Matthieu Totet <matthieu.totet@gmail.com>
2024-09-06 21:44:36 +0200

    All LC option

--- src/CommandLineArgs.h
+++ src/CommandLineArgs.h
@@ -8,42 +8,81 @@
 
 namespace CommandLineArgs
 {
+    enum NetworkMode {
+        SENDER,
+        GRABBER,
+        OFFLINE
+    };
     struct {
         bool skipDialog;
         const char* configFile;
         const char* shaderFile;
+        const char* serverURL;
+        NetworkMode networkMode;
     } Args;
 
     void parse_args(int argc,const char *argv[]) {
         Args.skipDialog = false;
         Args.configFile = "config.json";
         Args.shaderFile = "shader.glsl";
+        Args.networkMode = OFFLINE;
+        Args.serverURL = "ws://drone.alkama.com:9000/roomname/username";
         for(size_t i=0;i<argc;++i) {
 
             if(strcmp(argv[i],"skipdialog")==0){
                 Args.skipDialog = true;
+                continue;
             }
 
             if(strcmp(argv[i],"configfile")==0) {
                 i++;
                 assert_tuple_arg;
                 Args.configFile = argv[i];
+                continue;
             }
 
             if(strcmp(argv[i],"shader")==0) {
                 i++;
                 assert_tuple_arg;
                 Args.shaderFile = argv[i];
+                continue;
             }
+
+            if(strcmp(argv[i],"serverURL")==0) {
+                i++;
+                assert_tuple_arg;
+                Args.serverURL = argv[i];
+                continue;
+            }
+
+            if(strcmp(argv[i],"networkMode")==0) {
+                i++;
+                assert_tuple_arg;
+                if(strcmp(argv[i],"grabber")){
+                    Args.networkMode = GRABBER;
+                    continue;
+                }
+                if(strcmp(argv[i],"sender")){
+                    Args.networkMode = SENDER;
+                    continue;
+                }
+                if(strcmp(argv[i],"offline")){
+                    Args.networkMode = OFFLINE;
+                    continue;
+                }
+            }        
+
         }
     }
 
-    void apply_args_to_config(int argc,const char *argv[],const char **configFile ) {
-        parse_args(argc,argv);
+    void print_args_to_config() {
+
         fprintf(stdout,"skipDialog: %i\n",CommandLineArgs::Args.skipDialog);
         fprintf(stdout,"configFile: %s\n",CommandLineArgs::Args.configFile);
         fprintf(stdout,"shaderFile: %s\n",CommandLineArgs::Args.shaderFile);
-        *configFile = Args.configFile;
+        fprintf(stdout,"serverURL: %s\n",CommandLineArgs::Args.serverURL);
+        fprintf(stdout,"networkMode: %i\n",CommandLineArgs::Args.networkMode);
+
     }
 }
 
